e041317a02623c6ad34f0f730307fda261cd0b9c
rifanndi <rifandiannass@gmail.com>
2024-11-02 11:37:35 +0700

    first committeddaajesm

--- src/App.js
+++ src/App.js
@@ -3,17 +3,17 @@
 /* eslint-disable react/jsx-filename-extension */
 /* eslint-disable react/react-in-jsx-scope */
 /* eslint-disable-next-line no-unused-vars */
-import { Route, Routes } from 'react-router-dom';
+import { Route, Routes } from "react-router-dom";
 
-import LandingPage from 'pages/LandingPage';
-import ProjectPage from 'pages/ProjectPage';
-import NotFoundPage from 'pages/NotFoundPage';
-import TeamPage from 'pages/TeamPage';
+import LandingPage from "pages/LandingPage";
+import ProjectPage from "pages/ProjectPage";
+import NotFoundPage from "pages/NotFoundPage";
+import TeamPage from "pages/TeamPage";
 
-import { ProjectDetailPage } from 'pages/ProjectDetailPage';
-import { DiscussProjectPage } from 'pages/DiscussProjectPage';
+import { ProjectDetailPage } from "pages/ProjectDetailPage";
+import { DiscussProjectPage } from "pages/DiscussProjectPage";
 
-import './assets/css/styles.css';
+import "./assets/css/styles.css";
 
 function App() {
   return (
@@ -22,7 +22,7 @@ function App() {
         <Route exact path="/" element={<LandingPage />} />
         <Route path="/project" element={<ProjectPage />} />
         <Route exact path="/project/:id" element={<ProjectDetailPage />} />
-        <Route exact path="/team" element={<TeamPage />} />
+        <Route exact path="/tea" element={<TeamPage />} />
         <Route exact path="/discuss-project" element={<DiscussProjectPage />} />
         <Route path="**" element={<NotFoundPage />} />
       </Routes>
